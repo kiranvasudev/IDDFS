f315efe63a65e0feea861e459cad797ad5cc7503
patrickna <patrick.nagel@smail.inf.h-brs.de>
2016-05-05 08:57:16 +0200

    First idea is running. Mistakes are commented but not corrected, yet.

--- air_assignment_05/src/agent.cpp
+++ air_assignment_05/src/agent.cpp
@@ -55,59 +55,72 @@ void Agent::print_map(vector<vector<string> >& a_map) {
 
 bool Agent::recursive_dls(pair<int, int> current_node, int goal,
 		int current_level, int limit, vector<pair<int, int> > current_path) {
+	cout << "current level " << current_level << endl;
 
-//	current_path.push_back(current_node);
-//	std::string goalStr = std::to_string(goal);
+	// push the current node to the path
+	current_path.push_back(current_node);
 
+	// convert current goal-int in string to make it comparable with the current field value
+	std::string goalStr = std::to_string(goal);
+
+	// get the row and the column of the current node
 	int row = current_node.first;
 	int column = current_node.second;
 
-	cout << "row: " << row;
-	cout << "column: " << column;
-	if (map[row][column] == "1") {
-		cout << "goal found" << endl;
+	// if the current node is a goal node then call function backtrack_path (print path) and return true...
+	if (map[row][column] == goalStr) {
+		backtrack_path(current_path);
 		return true;
 
+		// ...otherwise mark the field. NOTE FOR MYSELF: is this necessary?
 	} else {
-		map[current_node.first][current_node.second] = "-";
+		map[row][column] = "-";
 	}
 
+	// if the level-limit is reached then return false...
 	if (current_level == limit) {
 		return false;
+
+		// ...otherwise check the children nodes
 	} else {
-		if (map[current_node.first][current_node.second + 1] == " "
-				|| map[current_node.first][current_node.second + 1]
-						== "1"/*should compare with the current goal 'goalStr'*/) {
-			recursive_dls(
-					make_pair(current_node.first, current_node.second + 1),
-					goal, current_level, limit, current_path);
+		// .. check the right child by calling the method with the child node and an increased level
+		if (map[row][column + 1] == " "
+				|| map[row][column + 1]
+						== goalStr/*should compare with the current goal 'goalStr'*/) {
+			recursive_dls(make_pair(row, column + 1), goal, current_level + 1,
+					limit, current_path);
 		}
-		if (map[current_node.first][current_node.second - 1] == " "
-				|| map[current_node.first][current_node.second - 1]
-						== "1"/*should compare with the current goal 'goalStr'*/) {
-			recursive_dls(
-					make_pair(current_node.first, current_node.second - 1),
-					goal, current_level, limit, current_path);
+
+		// .. check the left child by calling the method with the child node and an increased level
+		if (map[row][column - 1] == " "
+				|| map[row][column - 1]
+						== goalStr/*should compare with the current goal 'goalStr'*/) {
+			recursive_dls(make_pair(row, column - 1), goal, current_level + 1,
+					limit, current_path);
 		}
-		if (map[current_node.first + 1][current_node.second] == " "
-				|| map[current_node.first + 1][current_node.second]
-						== "1"/*should compare with the current goal 'goalStr'*/) {
-			recursive_dls(
-					make_pair(current_node.first + 1, current_node.second),
-					goal, current_level, limit, current_path);
+
+		// .. check the up child by calling the method with the child node and an increased level
+		if (map[row + 1][column] == " "
+				|| map[row + 1][column]
+						== goalStr/*should compare with the current goal 'goalStr'*/) {
+			recursive_dls(make_pair(row + 1, column), goal, current_level + 1,
+					limit, current_path);
 		}
-		if (map[current_node.first - 1][current_node.second] == " "
-				|| map[current_node.first - 1][current_node.second]
-						== "1"/*should compare with the current goal 'goalStr'*/) {
-			recursive_dls(
-					make_pair(current_node.first - 1, current_node.second),
-					goal, current_level, limit, current_path);
+
+		// ... check the down child by calling the method with the child node and an increased level
+		if (map[row - 1][column] == " "
+				|| map[row - 1][column]
+						== goalStr/*should compare with the current goal 'goalStr'*/) {
+			recursive_dls(make_pair(row - 1, column), goal, current_level + 1,
+					limit, current_path);
 		}
 	}
 
+	// NOTE FOR MYSELF: this part will be never reached ! (?)
+	backtrack_path(current_path);
 	number_of_visited_nodes++;
 	map[current_node.first][current_node.second] = "0";
-	return true;
+
 //TODO
 
 //Notes:
@@ -119,11 +132,27 @@ bool Agent::recursive_dls(pair<int, int> current_node, int goal,
 }
 
 bool Agent::depth_limited_seach(int limit) {
-	vector<pair<int, int> > current_path;
+	// start with the first goal, number 1
 	int goal = 1;
-	while (limit != max_limit || goal != number_of_goals) {
+
+	// run through the map until all goals are found or the limit reaches the max_limit
+	while (limit != max_limit and goal != number_of_goals) {
+
+		// make a (new) path for every iteration
+		vector<pair<int, int> > current_path;
+
+		// get a copy of an empty_map
+		map = empty_map;
+
+		// run through the map starting at the initial pos and a level 0.
+		/*NOTE FOR MYSELF: The goal will changed for every iteration.
+		 * It will never find a goal and stop after max 9 iteration <-- that's wrong and need to be changed.*/
 		recursive_dls(initial_pos, goal, 0, limit, current_path);
+
+		// increase the limit with every run. NOTE FOR MYSELF: Should be the whole map check for every goal, or in total?
 		limit += 1;
+
+		// increas the goal. That's not correct and need to be changed. Look at the previous notes
 		goal += 1;
 	}
 	return true;
@@ -144,6 +173,13 @@ void Agent::backtrack_path(vector<pair<int, int> > current_path) {
 	vector<vector<string> > local_map = empty_map;
 
 	pair<int, int> current_data;
+
+	cout << "path: " << endl;
+	for (unsigned i = 0; i < current_path.size(); ++i) {
+//		cout << current_path[i].first << ", " << current_path[i].second << endl;
+		current_data = current_path[i];
+		local_map[current_data.first][current_data.second] = "-";
+	}
 
 	//TODO
 	//Backtrace. Use the current path vector to set the path on the map.
